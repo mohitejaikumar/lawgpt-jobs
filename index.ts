80ee79ca6c1e738073258fd8756e88c7e8f670b2
mohitejaikumar <mohitjaikumar@gmail.com>
2024-07-18 11:34:21 +0530

    all good

--- index.ts
+++ index.ts
@@ -8,142 +8,118 @@ const uri = process.env.MONGODB_URI || "";
 async function main(){
 
     try{
-    await mongoose.connect(uri);
+        await mongoose.connect(uri);
     
-    // monthly and yearly refresh filter 
+        // monthly and yearly refresh filter 
         try{
         const refreshUsers = await User.aggregate([
-            {
-                // Add fields for the current date and the difference in days
-                $addFields: {
-                    currentDate: new Date(),
-                    daysDifference: {
-                        $divide: [
-                        { $subtract: [new Date(), "$purchaseDate"] },
-                        1000 * 60 * 60 * 24
-                        ]
+        {
+            $addFields: {
+                refreshDay:{
+                    
+                    $dayOfMonth: "$purchaseDate"
+                },
+                month : {$add: [new Date().getMonth(), 1]},
+                year : new Date().getFullYear(),
+                purchaseMonth : {
+                    $month : "$purchaseDate"
+                },
+                day: new Date().getDate()
+                
+            }
+        },
+        {
+            $addFields:{
+                maxDay:{
+                    $dayOfMonth : {
+                        $dateFromParts:{
+                        "year" : "$year",
+                        "day" : 0,
+                        "month" : {$add: ["$month", 1]}
+                        } 
+                    }
+                },
+            }
+        },
+        {
+            $addFields:{
+                newRefreshDay:{
+                    $cond:{
+                        if:{$gt:["$refreshDay","$maxDay"]},
+                        then: "$maxDay",
+                        else: "$refreshDay"
                     }
                 }
-            },
-            {
-                // Add a field to check if the difference in days is a multiple of 30
-                $addFields: {
-                    isMultipleOfMonth: { $gt: [{ $divide: ["$daysDifference", 30] }, 1] },
-                    months:{ 
-                        $divide:["$daysDifference", 30]
-                    }
-                }
-            },
-            {
-                // Match only documents where the difference is a multiple of 30 days
-                $match: { isMultipleOfMonth: true }
-            },
-            {
-                $group: {
-                    _id : "$plan",
-                    users :{
-                        $push : {
-                            id:"$_id",
-                            months: "$months"
-                        }
-                    }
-                }
-            },
-            ]);
+            }
+        },
+        {
+            $match:{
+            newRefreshDay : new Date().getDate()
+            }
+        },
+        {
+            $project:{
+                plan:1,
+                month:1,
+                purchaseMonth:1,
+                newRefreshDay:1,
+                purchaseYear:1,
+                year:1
+            }
+        }])
 
             let  promises:any[] = [];
-            refreshUsers.map((plan)=>{
-                
-                
-                if(plan._id === "essential"){
-                    promises.push(...plan.users.map((user)=>{
-                        if(user.months >=12){
-                            // yearly refresh
-                            return new Promise<void>(async (resolve,reject)=>{
-                                try{
-                                    await User.findByIdAndUpdate(user.id,{tokens:0});
+            refreshUsers.map((user)=>{
+                    if(user.month === user.purchaseMonth && user.year !== user.purchaseYear){
+                        // yearly refresh
+                        promises.push(new Promise<void>(async(resolve,reject)=>{
+                            try{
+                                    await User.findByIdAndUpdate(user._id,{tokens:0});
                                     resolve();
                                 }
                                 catch(err){
                                     reject();
                                 }
-                            })
-                        }
-                        else{
-                            // monthly refresh
-                            return new Promise<void>(async (resolve,reject)=>{
+                        }))
+                    }
+                    else{
+                        // monthly refresh
+                        if(user.plan === "essential"){
+                            promises.push(new Promise<void>(async(resolve,reject)=>{
                                 try{
-                                    await User.findByIdAndUpdate(user.id,{tokens:100000});
+                                    await User.findByIdAndUpdate(user._id,{tokens:100000});
                                     resolve();
                                 }
                                 catch(err){
                                     reject();
                                 }
-                            })
+                            }))
                         }
-                    }))
-                }
-                else if (plan._id === "growth"){
-                    promises.push(...plan.users.map((user)=>{
-                        if(user.months >=12){
-                            // yearly refresh
-                            return new Promise<void>(async (resolve,reject)=>{
+                        else if(user.plan === "growth"){
+                            promises.push(new Promise<void>(async(resolve,reject)=>{
                                 try{
-                                    await User.findByIdAndUpdate(user.id,{tokens:0});
+                                    await User.findByIdAndUpdate(user._id,{tokens:1000000});
                                     resolve();
                                 }
                                 catch(err){
                                     reject();
                                 }
-                            })
+                            }))
                         }
-                        else{
-                            // monthly refresh
-                            return new Promise<void>(async (resolve,reject)=>{
+                        else if(user.plan === "custom"){
+                            promises.push(new Promise<void>(async(resolve,reject)=>{
                                 try{
-                                    await User.findByIdAndUpdate(user.id,{tokens:1000000});
+                                    await User.findByIdAndUpdate(user._id,{tokens:3400000});
                                     resolve();
                                 }
                                 catch(err){
                                     reject();
                                 }
-                            })
+                            }))
                         }
-                    }))
-                }
-                else if (plan._id === "custom"){
-                    promises.push(...plan.users.map((user)=>{
-                        if(user.months >=12){
-                            // yearly refresh
-                            return new Promise<void>(async (resolve,reject)=>{
-                                try{
-                                    await User.findByIdAndUpdate(user.id,{tokens:0});
-                                    resolve();
-                                }
-                                catch(err){
-                                    reject();
-                                }
-                            })
-                        }
-                        else{
-                            // monthly refresh
-                            return new Promise<void>(async (resolve,reject)=>{
-                                try{
-                                    await User.findByIdAndUpdate(user.id,{tokens:3400000});
-                                    resolve();
-                                }
-                                catch(err){
-                                    reject();
-                                }
-                            })
-                        }
-                    }))
-                }
+                    }
             })
-
             await Promise.all(promises);
-            
-            
         }
         catch(err){
             console.error.bind(console, 'Refresh error:');
